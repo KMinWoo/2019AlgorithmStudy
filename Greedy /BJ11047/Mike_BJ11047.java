e702bee693a421fd211e6e43daf9fe34cc1be545
syli9526 <syli5806@gmail.com>
2019-03-28 18:24:25 +0900

    rm

--- Greedy /BJ11047/Mike_BJ11047.java
+++
@@ -1,9 +0,0 @@
-
-public class BJ11047 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
--- Greedy /BJ4796/Mike_BJ4796.java
+++
@@ -1,9 +0,0 @@
-
-public class BJ4796 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
